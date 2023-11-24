f6c30f6723d5d530f9763fe83c1d6f0fbcdb738f
AkshayB99 <akshaybharamshetti99@gmail.com>
2023-11-24 21:10:31 +0530

    Same as bellow

--- src/components/Choice0.jsx
+++ src/components/Choice0.jsx
@@ -153,7 +153,7 @@ function Choice0() {
   return (
     <>
       <div className="flex h-screen justify-center items-center">
-        <div className="m-auto bg-[#192A32] w-full sm:w-[415px] h-full sm:h-[650px] sm:rounded-[43px] flex justify-center absolute">
+        <div className="m-auto bg-[#192A32] w-full sm:w-[415px] h-screen sm:h-[650px] sm:rounded-[43px] flex justify-center absolute">
           <div className="sm:mt-[30%] mt-[50%] w-full">
             {/* icons */}
             <div className="flex mx-14 justify-between">
